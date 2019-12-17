4cd80ce85963559a474566999c0e3458fa0bbacd
Gabriel Tan-Chen <gabriel@gabrieltanchen.com>
2019-12-17 13:10:25 -0500

    Add background photo credits

--- app/controllers/innovation-nation-making-canada-a-leader-in-the-global-ideas-economy.js
+++ app/controllers/innovation-nation-making-canada-a-leader-in-the-global-ideas-economy.js
@@ -44,6 +44,10 @@ export default Controller.extend({
     }];
   }),
 
+  photoCredit: computed('intl.locale', function() {
+    return get(this, 'intl').t('innovationNation.photoCredit');
+  }),
+
   title: computed('intl.locale', function() {
     return get(this, 'intl').t('innovationNation.title');
   }),
