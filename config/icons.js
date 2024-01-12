6f254973e884275c2bacf4dbbf39142263029134
yang-wang-ds <yangwang7890@gmail.com>
2024-01-12 11:59:09 -0500

    update twitter icon to x

---
+++ config/icons.js
@@ -0,0 +1,27 @@
+module.exports = function() {
+  return {
+    'free-brands-svg-icons': [
+      'facebook-square',
+      'linkedin-in',
+      'x-twitter',
+    ],
+    'pro-light-svg-icons': [
+      'arrow-right',
+      'bars',
+      'camera-retro',
+      'chart-bar',
+      'chevron-left',
+      'chevron-right',
+      'comment-alt-lines',
+      'download',
+      'envelope',
+      'file-alt',
+      'film',
+      'newspaper',
+      'search',
+      'share-alt',
+      'times',
+      'volume-up',
+    ],
+  };
+};
